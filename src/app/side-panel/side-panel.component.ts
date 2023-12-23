22ff346f1fe01236495e54eac1077987c529703c
Editalthte <jordan.klinefelter@protonmail.com>
2023-12-23 13:11:03 -0600

    - Initial attempt at dynamic side panel toy

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { SidePanelService } from './side-panel.service';
 
 @Component({
   selector: 'app-side-panel',
@@ -9,5 +11,17 @@ import { Component } from '@angular/core';
 })
 export class SidePanel
 {
+	private panelList = Array.from(inject(SidePanelService).getPanels().values());
+	private currentPanelIndex = 0;
 
+	get currentPanel()
+	{ return this.panelList[this.currentPanelIndex]; }
+
+	displayNextPanel()
+	{
+		this.currentPanelIndex++;
+
+		if (this.currentPanelIndex >= this.panelList.length)
+		{ this.currentPanelIndex = 0; }
+	}
 }
--- src/app/side-panel/side-panel.service.ts
+++ src/app/side-panel/side-panel.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, Type } from '@angular/core';
+import { PanelBase } from './panels/panel-base/panel-base.component';
+import { HelloPanel } from './panels/hello-panel/hello-panel.component';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,45 @@ export class SidePanelService
 
 	getPanels()
 	{
+		const newPanelMap = new Map<string, any>(
+			[
+				[
+					"hello-placeholder",
+					{
+						component: HelloPanel,
+						inputs: { modeSetting: "placeholder", selectionType: "someSelection" }
+					}
+				],
 
+				[
+					"hello-content",
+					{
+						component: HelloPanel,
+						inputs: { modeSetting: "content", selectionType: "someDifferentSelection" }
+					}
+				]
+			]
+		)
+
+		return newPanelMap;
+
+		// as {component: Type<any>, inputs: Record<string, unknown>}[]
 	}
 }
+
+/**
+ * 	const newPanelMap = new Map<string, PanelItem>(
+		[
+			// ["categories", categoriesPanel],
+			// ["collaboration", collaborationPanel],
+			// ["editMetadata", editMetadataPanel],
+			// ["editNode", editNodePanel],
+			// ["miiv", miivPanel],
+			["evidence", evidencePanel],
+			["nodeStyle", nodeStylePanel]
+		]
+	);
+ */
 
 /**
  * import { Injectable, Type } from '@angular/core';
