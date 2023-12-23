ea33eb1af0a4e13cd233370456c119bc9c1818e0
Editalthte <jordan.klinefelter@protonmail.com>
2023-12-23 15:24:46 -0600

    - Side panel now uses Map instead of Array for dynamic components

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,28 +1,35 @@
 import { Component, inject } from '@angular/core';
-import { AsyncPipe, NgComponentOutlet } from '@angular/common';
+import { AsyncPipe, NgComponentOutlet, NgIf } from '@angular/common';
 
 import { SidePanelService } from './side-panel.service';
 
 @Component({
   selector: 'app-side-panel',
   standalone: true,
-  imports: [NgComponentOutlet, AsyncPipe],
+  imports: [NgComponentOutlet, AsyncPipe, NgIf],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.sass'
 })
 export class SidePanel
 {
-	private panelList = Array.from(inject(SidePanelService).getPanels().values());
+	// private panelList = Array.from(inject(SidePanelService).getPanels().values());
+	private panelList = inject(SidePanelService).getPanels();
+	private DEFAULT_PANEL_ID = "placeholder";
+	private _currentPanel = this.panelList.get(this.DEFAULT_PANEL_ID);
 	private currentPanelIndex = 0;
 
 	get currentPanel()
-	{ return this.panelList[this.currentPanelIndex]; }
-
-	displayNextPanel()
 	{
-		this.currentPanelIndex++;
+		// return this.panelList[this.currentPanelIndex];
+		return this._currentPanel;
+	}
 
-		if (this.currentPanelIndex >= this.panelList.length)
-		{ this.currentPanelIndex = 0; }
+	displayNextPanel(panelId: string)
+	{
+		this._currentPanel = this.panelList.get(panelId);
+		// this.currentPanelIndex++;
+
+		// if (this.currentPanelIndex >= this.panelList.length)
+		// { this.currentPanelIndex = 0; }
 	}
 }
