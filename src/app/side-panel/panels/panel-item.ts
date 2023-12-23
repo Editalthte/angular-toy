887de18286a9b453be17662324323e2459f30fb5
Editalthte <jordan.klinefelter@protonmail.com>
2023-12-23 15:43:51 -0600

    - Changed interfaces to classes

--- src/app/side-panel/panels/panel-item.ts
+++ src/app/side-panel/panels/panel-item.ts
@@ -1,14 +1,28 @@
 import { Type } from "@angular/core";
+import { PanelBase } from "./panel-base/panel-base.component";
 
-export interface PanelItemInputs
+export class PanelItemInputs
 {
 	modeSetting: string;
 	selectionType: string;
+
+	constructor(INIT_MODE_SETTING: string, INIT_SELECTION_TYPE: string)
+	{
+		this.modeSetting = INIT_MODE_SETTING;
+		this.selectionType = INIT_SELECTION_TYPE;
+	}
 }
 
-export interface PanelItem
+export class PanelItem
 {
 	id: string;
-	component: Type<any>;
+	component: Type<PanelBase>;
 	inputs: PanelItemInputs;
+
+	constructor(INIT_ID: string, INIT_COMPONENT: Type<PanelBase>, INIT_INPUTS: PanelItemInputs)
+	{
+		this.id = INIT_ID;
+		this.component = INIT_COMPONENT;
+		this.inputs = INIT_INPUTS;
+	}
 }
--- src/app/side-panel/side-panel.service.ts
+++ src/app/side-panel/side-panel.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Type } from '@angular/core';
 import { PanelBase } from './panels/panel-base/panel-base.component';
 import { HelloPanel } from './panels/hello-panel/hello-panel.component';
-import { PanelItem } from './panels/panel-item';
+import { PanelItem, PanelItemInputs } from './panels/panel-item';
 
 type DynamicComponentInputs = Record<string, unknown>;
 type DynamicComponentMap = Map<string, { component: Type<any>, inputs: DynamicComponentInputs }>;
@@ -16,18 +16,11 @@ export class SidePanelService
 	getPanels()
 	{
 		const helloPlaceholderPanel: PanelItem =
-		{
-			id: "hello-placeholder",
-			component: HelloPanel,
-			inputs: { modeSetting: "placeholder", selectionType: "someSelection" }
-		};
+			new PanelItem("hello-placeholder", HelloPanel, new PanelItemInputs("placeholder", "someSelection"));
 
 		const helloContentPanel: PanelItem =
-		{
-			id: "hello-content",
-			component: HelloPanel,
-			inputs: { modeSetting: "content", selectionType: "someDifferentSelection" }
-		}
+			new PanelItem("hello-content", HelloPanel, new PanelItemInputs("content", "someDifferentSelection"));
+
 
 		const newPanelMap = new Map<string, any>(
 			[
